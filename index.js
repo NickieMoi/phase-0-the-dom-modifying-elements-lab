b73744436a4f6bbf4266a0bb4f2bd1136a07ccf2
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-02 17:14:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Moringa School is the champion!";
+newHeader.className = "victory"; 
+newHeader.className = "victory";
